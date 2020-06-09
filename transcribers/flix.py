e73805267572efcd2e0283434efd4c2d93f47d6e
Tristan Blake <trisbla3@gmail.com>
2020-06-09 13:16:22 -0500

    Fixed scope and other minor errors

--- transcribers/flix.py
+++ transcribers/flix.py
@@ -3,6 +3,15 @@ from utils import get_pdf_text
 import requests
 import re
 
+BASE_URL = 'https://8flix.com'
+SHOW_TRANSCRIPTS_URL = 'https://8flix.com/transcripts/shows/'
+
+SHOWS_CSS_SELECTOR = '#menu-transcripts > li > div > a'
+SEASONS_CSS_SELECTOR = 'li.menu-item.fl-has-submenu > ul'
+EPISODES_CSS_SELECTOR = 'a'
+
+BUTTONS_CSS_SELECTOR = 'div.fl-button-wrap > a'
+PDF_BUTTON_INDEX = 0
 
 
 """
@@ -13,15 +22,7 @@ captions are available on 8flix.
 
 class FlixExtractor():
 
-    BASE_URL = 'https://8flix.com'
-    SHOW_TRANSCRIPTS_URL = 'https://8flix.com/transcripts/shows/'
-
-    SHOWS_CSS_SELECTOR = '#menu-transcripts > li > div > a'
-    SEASONS_CSS_SELECTOR = 'li.menu-item.fl-has-submenu > ul'
-    EPISODES_CSS_SELECTOR = 'a'
-
-    BUTTONS_CSS_SELECTOR = 'div.fl-button-wrap > a'
-    PDF_BUTTON_INDEX = 0
+    
 
     """
     title - String of title of Netflix show
@@ -35,12 +36,12 @@ class FlixExtractor():
 
         # Find show from title
         shows = soup.select(SHOWS_CSS_SELECTOR)
-        show = __find_show(title, shows)
+        show = self.__find_show(title, shows)
         show_details = show.parent.find_next_sibling('ul')
         
         # Find season
         szns = show_details.select(SEASONS_CSS_SELECTOR)
-        szn = szn[season - 1]
+        szn = szns[season - 1]
         episodes = szn.select(EPISODES_CSS_SELECTOR)
 
         # Find episode
@@ -49,7 +50,8 @@ class FlixExtractor():
         # Get episode link and fetch pdf
         episode_link = epis['href']
         e = requests.get(episode_link)
-        buttons = e.select(BUTTONS_CSS_SELECTOR)
+        pdf_soup = BeautifulSoup(e.content)
+        buttons = pdf_soup.select(BUTTONS_CSS_SELECTOR)
         pdf_url = BASE_URL + buttons[PDF_BUTTON_INDEX]['href']
 
         # Extract text from PDF
