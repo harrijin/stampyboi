bb184f6160e8faf9c82f68027f520c0123a62cfe
Harrison Jin <harrison.jin@utexas.edu>
2020-06-10 12:30:43 -0500

    added Netflix search to interface

--- interface/app.py
+++ interface/app.py
@@ -4,11 +4,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def render_index():
-    return(send_file("interface.html"))
+  return(send_file("interface.html"))
 
 @app.route('/results', methods=['POST'])
 def return_results():
-    quote = request.args['quote']
+  quote = request.args['quote']
+  if request.args['src'] == 'yt':
     source = request.args['source']
+    # Create an instance of YouTube here
     results = "Quote: " + quote + " Source: " + source + " Results: " 
-    return results
+  elif request.args['src'] == 'flix':
+    title = request.args['title']
+    szn = request.args['szn']
+    ep = request.args['ep']
+    # Create an instance of FlixExtractor here
+    results = "Quote: " + quote + " Show: " + title + " Season: " + szn + " Episode: " + ep + " Results: "
+  else:
+    results = "Error: Invalid searchSrc"
+
+  return results
