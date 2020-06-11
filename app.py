728c271d0d8923e1faf0a26390968534deb72d67
Harrison Jin <harrison.jin@utexas.edu>
2020-06-11 16:11:22 -0500

    interface.html -> searchPage.html + results.html, migrated logic from html to app.py, file upload functionality added

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
-import os
-from flask import Flask, send_file, request, flash, redirect, url_for
+import os, re
+from flask import Flask, send_file, request, flash, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from .transcribers.youtube import YouTube
 from .transcribers.flix import FlixExtractor
 
-UPLOAD_FOLDER = '/deepspeech/uploadedFiles'
+
+UPLOAD_FOLDER = './deepspeech/uploadedFiles'
 ALLOWED_EXTENSIONS = {'wav'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def ytVidId(url):
+    ytRegEx = re.compile('^(?:https?://)?(?:www\.)?(?:youtu\.be/|youtube\.com/(?:embed/|v/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$')
+    valid = ytRegEx.search(url)
+    if valid:
+        return valid.group(1)
+    return False
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def render_index():
-    return(send_file("interface.html"))
+    return render_template('searchPage.html')
 
 @app.route('/results', methods=['POST'])
 def return_results():
-    quote = request.args['quote']
-    if request.args['src'] == 'yt':
-        source = request.args['source'] # source is now a youtube video ID
-        transcriber = YouTube(source)
-        results = "Quote: " + quote + "<br>YouTube Video ID: " + source + "<br>Results: <br>" + str(transcriber.getTranscript())
-    elif request.args['src'] == 'flix':
-        title = request.args['title']
-        szn = request.args['szn']
-        ep = request.args['ep']
+    quote = request.form['quote']
+    
+    # =============YouTube=============
+    if request.form['search_src'] == 'yt':
+        source = request.form['yt_source']
+        if ytVidId(source):
+            transcriber = YouTube(ytVidId(source))
+            results = "Quote: " + quote + "<br>YouTube Video ID: " + ytVidId(source) + "<br>Results: <br>" + str(transcriber.getTranscript())
+        else:
+            results = "ERROR: Invalid YouTube link"
+    # =============Netflix==============
+    elif request.form['search_src'] == 'flix':
+        title = request.form['flix_title']
+        szn = request.form['flix_szn']
+        ep = request.form['flix_ep']
         try:
             transcriber = FlixExtractor(title, int(szn), int(ep))
             results = "Title: " + title + "<br>Season #: " + szn + "<br>Episode #: " + ep + "<br>Results: <br>" + str(transcriber.getTranscript())
@@ -35,19 +49,28 @@ def return_results():
             results = "ERROR: Netflix show " + title + " not found"
         except(IndexError):
             results = "ERROR:" + title +" season " + szn + " episode " + ep + " not found"
-    elif request.args['src'] == 'file':
-        if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file']
+    # ============File Upload===========
+    elif request.form['search_src'] == 'file':
+        # check if the post request has the file part
+        if 'vid_upload' not in request.files:
+            results = 'ERROR: No file part'
+            return render_template("results.html", result=results)
+        file = request.files['vid_upload']
+        # if user does not select file, browser also
+        # submit an empty part without filename
         if file.filename == '':
-            return 'No file selected' 
+            results = 'ERROR: No file selected'
+            return render_template("results.html", result=results)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            results = 'successful file upload'
+            results = 'successful file upload' # Change this to the search parameters concatenated to the timestamped transcript, like in the other two
         else:
-            return 'Invalid file format'
-    else:
-        results = "ERROR: Invalid searchSrc"
+            results = 'ERROR: incorrect file format'
 
-    return results
+    else:
+        results = "ERROR: Invalid searchsearch_src"
+
+    return render_template("results.html", result=results)
+
+
