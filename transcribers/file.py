b98eaf3df2fda6629052e9da02bca457339fdc78
Michael Cao <michaelc9264@gmail.com>
2020-06-23 20:13:15 -0500

    Add debug runner, remove prints

--- transcribers/file.py
+++ transcribers/file.py
@@ -18,6 +18,53 @@ class File(Transcriber):
         super().__init__()
         self.source = source
 
+    def getTranscript(self):
+        print('Source File: ' + self.source)
+        model = deepspeech.Model('deepspeech-0.7.3-models.pbmm')
+        model.enableExternalScorer('deepspeech-0.7.3-models.scorer')
+        rate_model = model.sampleRate()
+        print('Model SR: {}Hz'.format(rate_model))
+
+        extension = splitext(self.source)[1]
+        if extension in ['.ogv', '.mp4', '.mpeg', '.avi', '.mov']:
+            print('Extracting audio from video format '+extension)
+            audio, audio_length = video2Audio(self.source)
+        else:
+            wav = wave.open(self.source, 'rb')
+            rate_orig = wav.getframerate()
+            if rate_orig != rate_model:
+                audio = convert_samplerate(self.source, rate_model)
+            else:
+                audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
+            wav.close()
+            audio_length = wav.getnframes() * (1/rate_orig)
+
+            # print('Source SR: {}Hz'.format(rate_orig))
+
+        # timeElapsed = timer()
+        transcript = model.sttWithMetadata(audio, 1).transcripts[0]
+        # timeElapsed = timer() - timeElapsed
+
+        # print('Audio length: %.3f' % (audio_length))
+        # print('Time elapsed: %.3f' % (timeElapsed))
+        # print()
+        # print(''.join(token.text for token in metadata.tokens))
+        # print()
+
+        result = metadata_to_list(transcript)
+        return result
+
+def video2Audio(video_file):
+    '''Takes in any extension supported by ffmpeg: .ogv, .mp4, .mpeg, .avi, .mov, etc'''
+    audio = AudioFileClip(video_file, nbytes=2, fps=16000)
+    sound_array = audio.to_soundarray(fps=16000, quantize=True, nbytes=2)
+
+    if audio.nchannels == 2:
+        sound_array = sound_array.sum(axis=1) / 2
+        sound_array = sound_array.astype(np.int16)
+
+    return sound_array, audio.duration
+
 def convert_samplerate(audio_path, desired_sample_rate):
     sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate {} --encoding signed-integer --endian little --compression 0.0 --no-dither - '.format(quote(audio_path), desired_sample_rate)
     try:
@@ -46,55 +93,7 @@ def metadata_to_list(metadata):
 
     return result
 
-def metadata_to_text(metadata):
-    return ''.join(token.text for token in metadata.tokens)
-
-def getTranscript(self):
-    print('Source File: ' + source)
-    model = deepspeech.Model('deepspeech-0.7.3-models.pbmm')
-    model.enableExternalScorer('deepspeech-0.7.3-models.scorer')
-    rate_model = model.sampleRate()
-    print('Model SR: {}Hz'.format(rate_model))
-
-    extension = splitext(source)[1]
-    if extension in ['.ogv', '.mp4', '.mpeg', '.avi', '.mov']:
-        print('Extracting audio from video format '+extension)
-        audio, audio_length = video2Audio(source)
-    else:
-        wav = wave.open(source, 'rb')
-        rate_orig = wav.getframerate()
-        if rate_orig != rate_model:
-            audio = convert_samplerate(source, rate_model)
-        else:
-            audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
-        wav.close()
-        audio_length = wav.getnframes() * (1/rate_orig)
-
-        print('Source SR: {}Hz'.format(rate_orig))
-
-    timeElapsed = timer()
-    transcript = model.sttWithMetadata(audio, 1).transcripts[0]
-    timeElapsed = timer() - timeElapsed
-
-    print('Audio length: %.3f' % (audio_length))
-    print('Time elapsed: %.3f' % (timeElapsed))
-    print()
-    print(metadata_to_text(transcript))
-    print()
-
-    result = metadata_to_list(transcript)
-    result_json = json.dumps(result)
-    #print(json.dumps(result, sort_keys=True, indent=4))
-    return result, result_json
-
-def video2Audio(video_file):
-    '''Takes in any extension supported by ffmpeg: .ogv, .mp4, .mpeg, .avi, .mov, etc'''
-    audio = AudioFileClip(video_file, nbytes=2, fps=16000)
-    sound_array = audio.to_soundarray(fps=16000, quantize=True, nbytes=2)
-
-    if audio.nchannels == 2:
-        sound_array = sound_array.sum(axis=1) / 2
-        sound_array = sound_array.astype(np.int16)
-        print(sound_array)
-
-    return sound_array, audio.duration
+# DEBUG STUFF
+# trans = File('uploadedFiles/gettysburg.wav')
+# result = trans.getTranscript()
+# print(json.dumps(result, sort_keys=True, indent=4))
