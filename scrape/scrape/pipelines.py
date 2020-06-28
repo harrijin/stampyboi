5100f5c47b0b5aba461ff9a5c804c59cf749436d
Michael Cao <michaelc9264@gmail.com>
2020-06-28 16:58:32 -0500

    Add youtube download handling

--- scrape/scrape/pipelines.py
+++ scrape/scrape/pipelines.py
@@ -6,8 +6,10 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 from transcribers.youtube import YouTube
+from transcribers.file import FileExtractor
 from scrapy.exceptions import DropItem
 from datetime import datetime
+import youtube_dl
 
 class TranscriberPipeline:
 
@@ -25,18 +27,46 @@ class TranscriberPipeline:
     def process_item(self, item, spider):
         spider.all_count += 1
 
-        transcriber = YouTube(item["realid"])
+        id = item['realid']
+        transcriber = YouTube(id)
         transcript = transcriber.getTranscript()
         if transcript == "ERROR:YouTube video is unable to be searched, either because the video/captions are unavailable or the video is age-restricted.":
-            raise DropItem("No captions: %s" % item["realid"])
+            opts = {
+                'outtmpl' : '%(id)s.%(ext)s',
+                'postprocessors' : [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'wav',
+                }],
+                'logger': MyLogger(),
+                'cookiefile' : 'cookies.txt',
+            }
+            with youtube_dl.YoutubeDL(opts) as ydl:
+                ydl.download(['https://www.youtube.com/watch?v=' + id])
+
+            try:
+                with open(id + '.wav') as input:
+                    pass
+            except FileNotFoundError:
+                raise DropItem("No captions: " + id)
+
         else:
             spider.vid_count += 1
             print("COUNT: %d" % spider.vid_count)
             print("ALL  : %d" % spider.all_count)
             return {
-                'id' : item['realid'],
+                'id' : id,
                 'transcript' : transcript
             }
         # return item
 
         # TODO Fix the pipe to upload info to the database
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
