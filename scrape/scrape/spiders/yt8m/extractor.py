1bd187e2f2287a69e8c5415eebf7bd01acf9efd0
Michael Cao <michaelc9264@gmail.com>
2020-07-11 18:07:34 -0500

    Update space saving extract

--- scrape/scrape/spiders/yt8m/extractor.py
+++ scrape/scrape/spiders/yt8m/extractor.py
@@ -8,22 +8,32 @@ $ curl data.yt8m.org/download.py | partition=2/video/train mirror=us python
 
 import tensorflow as tf
 import numpy as np
-import os
+import os, subprocess
 
-filenames = ['train0093.tfrecord']
+def write(filenames, output):
+    raw_dataset = tf.data.TFRecordDataset(filenames)
+    def parse(proto):
+        return tf.io.parse_single_example(proto, {'id' : tf.io.FixedLenFeature([], tf.string)})
 
-# for file in os.listdir(os.getcwd() + "/records"):
-#     if file.endswith(".tfrecord"):
-#         filenames.append("records/" + file)
+    parsed_dataset = raw_dataset.map(parse)
 
-raw_dataset = tf.data.TFRecordDataset(filenames)
+    for features in parsed_dataset:
+        id_data = features['id'].numpy()
+        output.write(id_data.decode('utf-8') + '\n')
 
-output = open('../ids.txt', 'w')
-for raw_record in raw_dataset.take(-1): # grab all the records
-    example = tf.train.Example()
-    example.ParseFromString(raw_record.numpy()) # turn it into an example
-    id = str(example.features.feature['id'].bytes_list.value[0]) # extract the id feature (bytes)
-    id = id[2:6] # remove the byte stuff
+with open('ids.txt', 'w') as output:
+    for count in range(1, 101):
+        subprocess.run('cd records;curl data.yt8m.org/download.py | shard=%d,100 partition=2/video/train mirror=us python;rm *.json' % count, shell=True)
 
-    output.write(id + "\n")
-output.close()
+        filenames = []
+        for file in os.listdir(os.getcwd() + '/records'):
+            filenames.append('records/' + file)
+
+        write(filenames, output)
+
+        os.chdir('records')
+        for file in os.listdir(os.getcwd()):
+            os.remove(file)
+        os.chdir('..')
+
+        print('Finished shard ' + str(count))
