575e72bb566c52e254968dda6290dacc888b3613
Harrison Jin <harrison.jin@utexas.edu>
2020-07-31 17:37:37 -0500

    added new search bar to results page, updated style for spellcheck, added gif conversion

--- static/results.js
+++ static/results.js
@@ -1,6 +1,5 @@
 function check_spelling(){
-    document.getElementById('spellcheckButton').style='display:none';
-    var userIn=encodeURIComponent(document.getElementById('inputQuote').value);
+    var userIn=encodeURIComponent(document.getElementById('quote').value);
     var suggestRequest = new XMLHttpRequest();
     var suggestions;
     suggestRequest.open('POST','/spellcheck', true);
@@ -8,6 +7,7 @@ function check_spelling(){
     suggestRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             suggestions = JSON5.parse(this.responseText);
+            console.log(suggestions);
             if(suggestions.length > 0){
                 generateSpellcheck();
                 suggestions.forEach(addSuggestion);
@@ -21,9 +21,9 @@ function check_spelling(){
 function generateSpellcheck(){
     let prefix = document.createElement('span');
     let form = document.createElement('form');
-    let searchSrc = document.createElement('input');
     let dropdown = document.createElement('select');
     let submitButton = document.createElement('input');
+    let bottomBar = document.createElement('span');
     prefix.style = 'color:red';
     prefix.innerHTML = "Did you mean:";
     form.action = '/results';
@@ -31,16 +31,11 @@ function generateSpellcheck(){
     form.enctype = 'application/x-www-form-urlencoded';
     form.method = 'POST';
     form.style = 'display:inline';
-    searchSrc.type = 'text';
-    searchSrc.id='search_src';
-    searchSrc.style='display:none'; // this line BAD (limits searching to only solr search, can't set additional options)
-    searchSrc.value='none';
-    searchSrc.name='search_src';
-    dropdown.id='quote';
+    dropdown.id='suggestions';
     dropdown.name='quote';
+    dropdown.classList.add('select-text');
     submitButton.type='submit';
     submitButton.value='Search';
-    form.appendChild(searchSrc);
     form.appendChild(dropdown);
     form.appendChild(submitButton);
     document.getElementById('spellchecking').appendChild(prefix);
@@ -52,7 +47,7 @@ function addSuggestion(suggestion){
     suggestion = suggestion.substring('script:'.length);//gets rid of 'script:'' from query
     option.value=suggestion;
     option.innerHTML=suggestion;
-    document.getElementById("quote").appendChild(option);
+    document.getElementById("suggestions").appendChild(option);
 }
 
 function directToVideo(info, stampIndex) {
@@ -60,7 +55,7 @@ function directToVideo(info, stampIndex) {
     console.log(info)
     $.ajax({
         url: '/video',
-        type: 'POST', 
+        type: 'POST',
         data: JSON.stringify(info),
         contentType: 'application/json',
         success: function(response) {
