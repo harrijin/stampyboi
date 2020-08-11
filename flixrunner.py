0963a760a13fb2a1b7d8a6955325639f65269900
Michael Cao <michaelc9264@gmail.com>
2020-08-11 16:15:01 -0500

    Add flixrunner

---
+++ flixrunner.py
@@ -0,0 +1,31 @@
+from transcribers.flix import FlixExtractor
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+import os, pysolr
+
+SOLR_HOST_DIR = '/Documents'
+WORKING_DIRECTORY = os.getcwd()
+HOME = str(Path.home())
+os.chdir(HOME + SOLR_HOST_DIR)
+file = open("solrhost.txt", "r")
+SOLR_HOST = str(file.read())
+os.chdir(WORKING_DIRECTORY)
+
+solr = pysolr.Solr('http://'+SOLR_HOST+'/solr/stampyboi')
+
+def extract(videoID):
+    print(f'START   {videoID}')
+    transcriber = FlixExtractor(videoID)
+    transcriptJSON = transcriber.convertToJSON()
+    print(f'EXTRACT {videoID}')
+    solr.add(transcriptJSON, commit=True)
+    print(f'DONE    {videoID}')
+    
+executor = ThreadPoolExecutor()
+
+with open('flix.txt') as f:
+    for line in f:
+        id = line.rstrip()
+        executor.submit(extract, id)
+
+executor.shutdown()
